4c1821f78f186ff172eaf4737becb3a79e7df710
leviosaz <86320194+leviosaz@users.noreply.github.com>
2022-06-22 16:27:38 -0700

    Create 1-twosum.py

---
+++ 1-twosum.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        difference = {}
+
+        for x in range(len(nums)):
+            diff = target - nums[x]
+            if difference.get(diff) != None:
+                return [x, difference[diff]]
+            else:
+                difference[nums[x]] = x 
+
+                
+# Time Complexity: O(n)
+# Space Complexity: O(n)
