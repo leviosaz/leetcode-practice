7f9d7170dbe7cced3206863ebdd3d9100fb8a958
leviosaz <86320194+leviosaz@users.noreply.github.com>
2022-07-02 09:56:01 -0700

    Add files via upload

---
+++ 20-valid_parens.py
@@ -0,0 +1,23 @@
+# Check if the input holds valid parentheses pairs.
+
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+
+        d = {"(":")", "{":"}", "[":"]"}
+
+        for x in s:
+            if x in "({[":
+                stack.append(x)
+            else:
+                if not stack:
+                    return False
+
+                current = stack.pop()
+
+                if current in "({[" and d[current] != x:
+                    return False
+
+        if stack:
+            return False
+        return True
