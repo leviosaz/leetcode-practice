53bb0121ea2db12fbf4fab005fd6dee6f7462900
leviosaz <86320194+leviosaz@users.noreply.github.com>
2022-07-10 19:00:49 -0700

    Create 0206-reverse_linked_list.py

---
+++ 0206-reverse_linked_list.py
@@ -0,0 +1,27 @@
+# Problem: Reverse a linked list.
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
+        prev = None
+        curr = head
+        
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+        return prev
+            
+# Time Complexity: O(n)
+# Space Complexity: O(1)
